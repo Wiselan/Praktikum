881219b42b0ff47aa00b4ac1c8df37d57ecb8ba0
Wiselan <daffamaheswara32@gmail.com>
2024-08-01 14:38:04 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Moviecontroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [MovieController::class, 'index']);
